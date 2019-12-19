c3c785708aef9938db8f90a14d1d416451547265
ilya.rapoports <ilya.rapoports@accenture.com>
2019-12-19 09:44:15 +0200

    add Entity

--- src/main/java/com/clinic/meler/model/TimeTable.java
+++ src/main/java/com/clinic/meler/model/TimeTable.java
@@ -4,22 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "images")
-public class Images {
+public class TimeTable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column
-    private byte[] img;
-@Column
-private String description;
-
-    @ManyToOne
+    private Date dateTime;
+    @OneToMany
+    private Dentist dentist;
+    @OneToMany
     private Patient patient;
 
 }
